2426694bdbdded59cd288ca4bc6ae04e89ca4ee6
shuhat <shuhat1971@gmail.com>
2021-08-05 17:33:51 +0600

    new landing page  graphics added

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -11,19 +11,19 @@ function Landing() {
                 </div>
                 {/* <div className="text-6xl font-semibold mt-3">Shuhat</div>
                 <div className="text-xl	mt-3">I'm a <span className="font-bold">Software Developer</span></div> */}
-                <button className="mt-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <button className="mt-8 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
                     <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                     <span><form method="get" action="Kh Shuhat Uddin.pdf">
                         <button type="submit">Download My CV</button>
                     </form></span>
                 </button>
-                <div className="mt-6 mb-4 flex flex-row justify-center gap-x-12 relative">
+                <div className="mt-10 mb-4 flex flex-row justify-center gap-x-12 relative">
                     <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://github.com/zipped36"><i className="fab fa-github fa-2x mx-auto"/></a></div>
                     <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://stackoverflow.com/users/14694891/shuhat36"><i className="fab fa-stack-overflow fa-2x"/></a></div>
                     <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://www.linkedin.com/in/khshuhatuddin/"><i className="fab fa-linkedin-in fa-2x"/></a></div>
                     <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://dev.to/"><i className="fab fa-dev fa-2x"/></a></div>
                 </div>
-                <img src="color pencil.png" className="overflow-hidden"></img>
+                <img src="color pencil.png" className="float-right"></img>
             </div>
         </div>
     )
--- src/Components/LandingProject.js
+++ src/Components/LandingProject.js
@@ -2,20 +2,21 @@ import React from 'react'
 
 function LandingProject() {
     return (
-        <div className="h-screen bg-green-50 pt-64 z-0">
+        <div className="h-screen bg-green-50 pt-48 z-0">
             <div className="container mx-auto text-center">
                 <div className="text-3xl font-semibold mt-3">Can't thank you enough for visiting</div>
                 <div className="text-5xl font-semibold mt-6">'My Projects'</div>
-                    <div className="mt-16 self-start flex flex-row justify-center gap-x-10">
-                        <div className="bg-white p-4 h-16 w-16 rounded-full hover:shadow-xl cursor-pointer"><i className="fab fa-github fa-2x"/></div>
-                        <div className="bg-white p-4 h-16 w-16 rounded-full hover:shadow-xl cursor-pointer"><i className="fab fa-linkedin-in fa-2x"/></div>
-                        <div className="bg-white p-4 h-16 w-16 rounded-full hover:shadow-xl cursor-pointer"><i className="fab fa-stack-overflow fa-2x"/></div>
-                        <div className="bg-white p-4 h-16 w-16 rounded-full hover:shadow-xl cursor-pointer"><i className="fab fa-dev fa-2x"/></div>
-                    </div>
-                    <div className="flex mt-24 mb-24 justify-center gap-x-6">
+                <img src="projectlanding.png" className="h-96 w-5/12 mx-auto mt-12"></img>
+                <div className="mt-6 mb-4 flex flex-row justify-center gap-x-12 relative">
+                    <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://github.com/zipped36"><i className="fab fa-github fa-2x mx-auto"/></a></div>
+                    <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://stackoverflow.com/users/14694891/shuhat36"><i className="fab fa-stack-overflow fa-2x"/></a></div>
+                    <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://www.linkedin.com/in/khshuhatuddin/"><i className="fab fa-linkedin-in fa-2x"/></a></div>
+                    <div className="bg-white h-14 w-14 p-2.5 rounded-full hover:shadow-xl cursor-pointer"><a href="https://dev.to/"><i className="fab fa-dev fa-2x"/></a></div>
+                </div>
+                    {/* <div className="flex mt-24 mb-24 justify-center gap-x-6">
                         <div className="text-xl font-bold self-center">scroll down please</div>
                         <span style={{color: "#059669"}}><i class="fas fa-arrow-circle-down fa-7x"></i></span>
-                    </div>
+                    </div> */}
                     <img src="color pencil.png" className="float-right"></img> 
                 </div>
             </div>
